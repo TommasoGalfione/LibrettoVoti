66fad0fb552d3ef8384e70cf906435e21c5caeb7
tommy <tommy@DESKTOP-F1M6065.homenet.telecomitalia.it>
2021-03-08 11:20:35 +0100

    soluzione punto 2

--- src/main/java/model/Libretto.java
+++ src/main/java/model/Libretto.java
@@ -13,6 +13,42 @@ public class Libretto {
 		this.voti.add(v);
 		
 	}
+	/*
+	public void stampaVotiUguali(int punteggio) {
+		//libretto stampa solo i voti
+	}
+	
+	public String votiUgauali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il chiamante a stamparli
+	}
+	*/
+	
+	public List<Voto> listaVotiUguali(int punteggio){
+		List<Voto> risultato= new ArrayList<>();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+			
+		}
+		return risultato;
+	}
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato =new Libretto();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+				//risultato.voti.add(v);
+			}
+		
+			
+		}
+		return risultato;
+		
+	}
+
 	public String toString() {
 		String s="";
 		for(Voto v: this.voti) {
--- src/main/java/model/TestLibretto.java
+++ src/main/java/model/TestLibretto.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class TestLibretto {
 	public static void main(String[] args) {
@@ -12,10 +13,16 @@ public class TestLibretto {
 		Voto voto1= new Voto("Analisi 1",30, LocalDate.of(2019, 2, 15));
 		
 		libretto.add(voto1);
+		libretto.add(new Voto("chimica",25,LocalDate.of(2019, 3, 19)));
 		libretto.add(new Voto("Fisica 1",28,LocalDate.of(2019, 2, 19)));
 		libretto.add(new Voto("informatica",24,LocalDate.of(2019, 9, 19)));
 		
 		System.out.println(libretto);
+		List<Voto> venticinque= libretto.listaVotiUguali(25);
+		System.out.println(venticinque);
+		
+		Libretto librettoVenticinque =libretto.votiUguali(25);
+		System.out.println(librettoVenticinque);
 	}
 
 }
